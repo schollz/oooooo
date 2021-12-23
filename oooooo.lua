e02534883f7cce92a9fa10c5124c44842a710c58
Zack Scholl <zack.scholl@gmail.com>
2021-12-23 08:47:28 -0800

    defaults

--- oooooo.lua
+++ oooooo.lua
@@ -1,4 +1,4 @@
--- oooooo v1.9.1
+-- oooooo v1.9.1 - monosong
 -- 6 x digital tape loops
 --
 -- llllllll.co/t/oooooo
@@ -111,9 +111,9 @@ function init()
   params:set_action("load on start",update_parameters)
   params:add_option("play on start","play on start",{"no","yes"},1)
   params:set_action("play on start",update_parameters)
-  params:add_option("start lfos random","start lfos random",{"no","yes"},1)
+  params:add_option("start lfos random","start lfos random",{"no","yes"},2)
   params:set_action("start lfos random",update_parameters)
-  params:add_control("start length","start length",controlspec.new(0,64,'lin',1,0,'beats'))
+  params:add_control("start length","start length",controlspec.new(0,64,'lin',1,16,'beats'))
   params:set_action("start length",update_parameters)
   params:add_option("use kolor","use kolor (restart req)",{"no","yes"},1)
   params:set_action("use kolor",update_parameters)
